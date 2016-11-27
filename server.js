8aa538fed858f37b1e424c79f80ac8cf8d32d523
DucPT <ducpt1012@gmail.com>
2016-11-27 14:50:21 +0700

    add models, views

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-var express = require('express')
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log("Okie, server is going on "+ port);
+exports = module.exports = app;
