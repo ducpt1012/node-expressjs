ab441c8fa4dd870173c6965d11e75b5124210f49
DucPT <ducpt1012@gmail.com>
2016-11-27 14:56:50 +0700

    complete back-end

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
-
-
-
-
-
-
+app.use(express.static(__dirname+'/public'));
+require('./app/routes')(app);
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log("Okie, server is going on "+ port);
